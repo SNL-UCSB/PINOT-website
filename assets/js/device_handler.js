6729b8aee3a28d92a68a00cac56c7c8b77e61aa6
maybe-hello-world <maybe.hello.world@gmail.com>
2023-04-06 20:39:36 -0700

    add live chart

--- assets/js/device_handler.js
+++ assets/js/device_handler.js
@@ -3,7 +3,7 @@ function getQueryParam(name) {
     return urlParams.get(name);
 }
 
-const eth_mac = encodeURIComponent(getQueryParam('device_label'));
+const eth_mac = encodeURIComponent(getQueryParam('label'));
 
 $(document).ready(function () {
     // Fetch data from the API using AJAX
@@ -12,7 +12,8 @@ $(document).ready(function () {
         method: 'GET',
         dataType: 'json',
         success: function (data) {
-            console.log(data)
+            init_plot(data);
+            console.log(data);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.error('Error fetching data:', textStatus, errorThrown);
@@ -20,3 +21,54 @@ $(document).ready(function () {
     });
 });
 
+function extractAverageRTT(inputString) {
+    const regex = /rtt min\/avg\/max\/mdev = [\d.]+\/([\d.]+)\/[\d.]+\/[\d.]+ ms/;
+    const match = inputString.match(regex);
+
+    if (match && match[1]) {
+        return parseFloat(match[1]);
+    } else {
+        throw new Error('Invalid input string format');
+    }
+}
+
+function init_plot(data) {
+
+    const ctx = document.getElementById('deviceChar').getContext('2d');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.map(d => d.time),
+            datasets: [
+                {
+                    label: 'Signal',
+                    data: data.map(d => d.signal),
+                    borderColor: 'blue',
+                    yAxisID: 'y1',
+                },
+                {
+                    label: 'Ping',
+                    data: data.map(d => extractAverageRTT(d.ping)),
+                    borderColor: 'red',
+                    yAxisID: 'y2',
+                },
+
+            ]
+        },
+        options: {
+            scales: {
+                y1: {
+                    type: 'linear',
+                    position: 'left',
+                    min: -128,
+                    max: 0
+                },
+                y2: {
+                    type: 'linear',
+                    position: 'right',
+                },
+            },
+        },
+    });
+}
